4e476f86ab0a7ccb6bce180170227710a97c817b
Lucas Campos <bhlucascampos@gmail.com>
2023-12-11 22:38:31 -0300

    insert classificationrlm component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ClassificationrlmComponent } from '../../components/classificationrlm/classificationrlm.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ClassificationrlmComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
